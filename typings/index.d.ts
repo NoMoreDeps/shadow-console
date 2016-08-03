408edbf1da3566891f47d54abd57706c589fb561
fskorzec <francois@skorzec.fr>
2016-08-03 22:40:11 +0200

    removed typings directory

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/jest/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/pngjs2/index.d.ts" />
-/// <reference path="modules/es6-promise/index.d.ts" />
